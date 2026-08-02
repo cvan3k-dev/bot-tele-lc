1d66435b64babe6a048ddd5a72ba640e16804f71
cvan3k-dev <cvan3k@gmail.com>
2026-08-02 09:26:24 +0700

    Update bot.py
    vip

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandle
 # ═══════════════════════════════════════════════════════════════════
 #  CẤU HÌNH CAO CẤP
 # ═══════════════════════════════════════════════════════════════════
-BOT_TOKEN = os.getenv("BOT_TOKEN", "fallback_token_here")
+BOT_TOKEN   = "8774993011:AAHM3uCpCqlaOTRdOIL1mDU-JGDkdLT78sA"
 ADMIN_IDS   = [5888859004]
 API_URL     = "https://wtxmd52.tele68.com/v1/txmd5/sessions"
 SYNC_SEC    = 5                      # Đồng bộ mỗi 5 giây
